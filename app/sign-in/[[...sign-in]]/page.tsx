29b74c137e3c8c340eba2fdc99b75d02328473eb
Khalil@whirble <elkortobikhalil614@gmaile.com>
2024-03-01 17:38:10 +0100

    landing update

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-800">
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-screen h-screen flex items-center justify-center bg-gray-800">
         <SignIn />
     </div>
   );
