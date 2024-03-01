ee99b11fa44f7300c4787b445025903470a2ecf0
Khalil@whirble <elkortobikhalil614@gmaile.com>
2024-03-01 17:53:12 +0100

    landing update

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
   return (
-    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-screen h-screen flex items-center justify-center bg-gray-800">
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-screen h-screen flex items-center justify-center p-0 bg-gray-800 bg-img-pattern bg-cover bg-no-repeat">
         <SignIn />
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -71,6 +71,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      backgroundImage: {
+        'img-pattern': "url('https://picsum.photos/200/300')",
+       },
     },
   },
   plugins: [require("tailwindcss-animate")],
